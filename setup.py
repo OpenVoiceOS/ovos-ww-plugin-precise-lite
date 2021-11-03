8798b2688db61a34138b43c36f0108e4b7bfd137
jarbasai <jarbasai@mailfence.com>
2021-11-03 20:29:58 +0000

    0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 PLUGIN_ENTRY_POINT = 'ovos-precise-lite=ovos_ww_plugin_precise_lite:PreciseLiteHotwordPlugin'
 setup(
     name='ovos-ww-plugin-precise-lite',
-    version='0.1.0a4',
+    version='0.1.0',
     description='A wake word plugin for OpenVoiceOS',
     url='https://github.com/OpenVoiceOS/ovos-ww-plugin-precise-lite',
     author='JarbasAi',
@@ -13,7 +13,7 @@ setup(
     license='Apache-2.0',
     packages=['ovos_ww_plugin_precise_lite'],
     install_requires=["precise_lite_runner>=0.3.2",
-                      "ovos-plugin-manager>=0.0.1a7",
+                      "ovos-plugin-manager>~0.0.2",
                       "pyxdg>=0.26"],
     zip_safe=True,
     classifiers=[
